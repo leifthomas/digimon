854c85e3a2d3e8501b44699e3fe46c0ff8e7393d
leifthomas <leifthomas@users.noreply.github.com>
2026-07-01 13:34:39 -0400

    Move DigimonDataService to shared folder so it can be used in state management

--- src/app/shared/services/digimon-data/digimon-data.service.ts
+++ src/app/shared/services/digimon-data/digimon-data.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 
-import { DigimonListItem, Page } from '../../../../shared';
+import { DigimonListItem, Page } from '../../../shared';
 
 @Service()
 export class DigimonDataService {
     readonly #httpClient = inject(HttpClient);
     readonly #url = 'https://digi-api.com/api/v1/digimon';
 
-    getDigimon() {
+    getDigimonPage() {
         return this.#httpClient.get<Page<DigimonListItem>>(this.#url);
     }
 }
